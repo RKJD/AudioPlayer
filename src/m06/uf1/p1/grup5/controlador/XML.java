5d6b7e51321a84f216b930a4a35c07c4d244896d
Marcopole <nicomarcopernic@gmail.com>
2019-05-01 19:12:20 +0200

    Cargando Playlist del xml, pero esta vez sin errores

--- src/m06/uf1/p1/grup5/controlador/XML.java
+++ src/m06/uf1/p1/grup5/controlador/XML.java
@@ -5,6 +5,7 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import javax.xml.parsers.*;
+import m06.uf1.p1.grup5.modelo.AudioList;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
@@ -12,7 +13,8 @@ public class XML {
 
     private String archivo = "audios/canciones.xml";
     public Map<Integer, Cancion> MapCanciones;
-
+    public Map<Integer, Playlist> MapPlaylist;
+    
     public void cargarCanciones() throws FileNotFoundException, IOException, ParserConfigurationException {
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -23,8 +25,7 @@ public class XML {
 
             document = builder.parse(new File(archivo));
             Element arrel = document.getDocumentElement();
-            NodeList arrel.getElementsByTagName("canciones");
-            NodeList data = arrel.getChildNodes();
+            NodeList listaCanciones = arrel.getElementsByTagName("canciones");
             
             for (int i = 0; i < listaCanciones.getLength(); i++) {
                 Node node = listaCanciones.item(i);
@@ -53,4 +54,52 @@ public class XML {
         }
     }
 
+    public void cargarListas() throws FileNotFoundException, IOException, ParserConfigurationException {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        Document document;
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        try {
+
+            document = builder.parse(new File(archivo));
+            Element arrel = document.getDocumentElement();
+            NodeList listaPlaylist = arrel.getElementsByTagName("playlist");
+            
+            for (int i = 0; i < listaPlaylist.getLength(); i++) {
+                Node node = listaPlaylist.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element canciones = (Element) node;
+                    int id = Integer.parseInt(canciones.getAttribute("id"));
+                    Element nom = (Element) canciones.getElementsByTagName("nom").item(0);
+                    Element ruta = (Element) canciones.getElementsByTagName("ruta").item(0);
+
+                    Playlist pl = new Playlist(id,
+                            nom.getChildNodes().item(0).getNodeValue(),
+                            ruta.getChildNodes().item(0).getNodeValue());
+                    MapPlaylist.put(id, pl);
+                }
+            }
+        } catch (SAXException ex) {
+            ex.printStackTrace();
+        }
+    }
+    private class Playlist{
+        private int id;
+        private String nom,ruta;
+        public Playlist(int id, String nom, String ruta){
+            this.id = id;
+            this.nom = nom;
+            this.ruta = ruta;
+        }
+        public int getId() {
+            return id;
+        }
+        public String getNom() {
+            return nom;
+        }
+        public String getRuta() {
+            return ruta;
+        }
+    }
 }
