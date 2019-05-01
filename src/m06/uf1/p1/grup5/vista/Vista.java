0fe01c63eb4a93f56f15cfc9dfaed85d363cab53
RKJD <44412937+RKJD@users.noreply.github.com>
2019-05-01 21:49:26 +0200

    Rellenando la tabla con las canciones de playlist

--- src/m06/uf1/p1/grup5/vista/Vista.java
+++ src/m06/uf1/p1/grup5/vista/Vista.java
@@ -95,9 +95,11 @@ public class Vista {
         for (int i = 0; i != 15; i++) {
             lista.setValueAt(i + 1, i, 0);
         }
+        /*
         for (int x = 0; x != 15; x++) {
-            //lista.setValueAt(cont.getCancion(x).getNom(), x, 1);
+            lista.setValueAt(cont.getCancion(x).getNom(), x, 1);
         }
+        */
 
         System.out.println(lista.getRowCount() + " " + lista.getColumnCount());
         lista.setShowVerticalLines(true);
@@ -250,5 +252,13 @@ public class Vista {
         this.nombreCancion.setText(info.getNom());
         this.autor.setText(info.getAutor());
     }
+    
+    public void updateSongsStart(String[] info){
+        for (int x = 0; x < info.length; x++) {
+            lista.setValueAt(info[x], x, 1);
+        }
+    }
 //</editor-fold>
+    
+    
 }
