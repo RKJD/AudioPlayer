7b2637b1018c342b512688e08c3d0d41afa2bc0c
Marcopole <nicomarcopernic@gmail.com>
2019-05-01 14:47:58 +0200

    Nuevos campos en las listas de reproducción con sus getters

--- src/m06/uf1/p1/grup5/modelo/AudioList.java
+++ src/m06/uf1/p1/grup5/modelo/AudioList.java
@@ -5,6 +5,8 @@
  */
 package m06.uf1.p1.grup5.modelo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Random;
 
 /**
@@ -12,12 +14,27 @@ import java.util.Random;
  * @author Marc
  */
 public class AudioList {
+    private String name,desc, imgPath;
     private int[] tracks;
     private int currentSong;
-    public AudioList(int[] tracks){
+    public AudioList(String name, String description, int[] tracks){
+        defineValues(name, description, tracks);
+        imgPath = null;
+    }
+    public AudioList(String name, String description, String picture, int[] tracks){
+        defineValues(name, description, tracks);
+        imgPath = picture;
+    }
+    private void defineValues(String name, String description, int[] tracks){
+        this.name = name;
+        this.desc = description;
         this.tracks = tracks;
         currentSong = tracks.length-1;
     }
+    /***@return Devuelve el nombre de la lista*/
+    public String getName(){return name;}
+    /***@return Devuelve la descripción de la lista*/
+    public String getDescription(){return desc;}
     /***@return Devuelve el orden de ids en formato de array*/
     public int[] getTracks(){return tracks;}
     /***@return Devuelve el id de una canción de la lista en la posición especificada*/
@@ -38,6 +55,18 @@ public class AudioList {
     }
     /***@return Devuelve el id de una canción aleatória de la lista*/
     public int getTrack(Random rand){return tracks[(int)(rand.nextFloat()*tracks.length)];}
-    public void test(){
+    /***
+     * Comprueba si la lista tiene una imagen y el archivo existe.
+     * @exception NoSuchFieldException si esta lista no tiene imagen
+     * @exception FileNotFoundException si el archivo no existe
+     * @return Devuelve el archivo si existe
+     */
+    public File getImage() throws NoSuchFieldException, FileNotFoundException{
+        if(imgPath.equals(null)) throw new NoSuchFieldException("This album have no Picture");
+        else{
+            File retorno = new File(imgPath);
+            if(retorno.exists()) return retorno;
+            else throw new FileNotFoundException("This file doesn't exist");
+        }
     }
 }
