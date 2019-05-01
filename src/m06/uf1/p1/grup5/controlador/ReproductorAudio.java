ca16dbb3ecbf22c7d3f2eea49f13d3a1323b9bda
RKJD <44412937+RKJD@users.noreply.github.com>
2019-05-01 13:36:12 +0200

    lee las 15 canciones pero salen null

--- src/m06/uf1/p1/grup5/controlador/ReproductorAudio.java
+++ src/m06/uf1/p1/grup5/controlador/ReproductorAudio.java
@@ -11,9 +11,21 @@ package m06.uf1.p1.grup5.controlador;
  */
 import m06.uf1.p1.grup5.controlador.Controlador;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class ReproductorAudio {
     public static void main(String[] args) throws FileNotFoundException {
-        Controlador controlador = new Controlador();     
+        try {
+            leerXML a = new leerXML();
+            a.cargarCanciones();
+            //Controlador controlador = new Controlador();     
+        } catch (IOException ex) {
+            Logger.getLogger(ReproductorAudio.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(ReproductorAudio.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- src/m06/uf1/p1/grup5/controlador/leerXML.java
+++ src/m06/uf1/p1/grup5/controlador/leerXML.java
@@ -3,19 +3,41 @@ package m06.uf1.p1.grup5.controlador;
 import java.io.*;
 import javax.xml.parsers.*;
 import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 public class leerXML {
 
-    public static void main(String[] args) {
+    private String archivo = "audios/canciones.xml";
+
+    public void cargarCanciones() throws FileNotFoundException, IOException, ParserConfigurationException {
+
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        Document documento = null;
+        Document document;
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
         try {
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            documento = builder.parse( new File("audios/canciones.xml"));
-            documento.getNodeName();
-            System.out.println(documento.getDoctype());
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            document = builder.parse(new File(archivo));
+            Element arrel = document.getDocumentElement();
+            NodeList listaCanciones = arrel.getChildNodes();
+            for (int i = 0; i < listaCanciones.getLength(); i++) {
+                Node node = listaCanciones.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element canciones = (Element) node;
+                    Element ruta = (Element) canciones.getElementsByTagName("ruta").item(0);
+                    Element autor = (Element) canciones.getElementsByTagName("autor").item(0);
+
+                    Cancion canc = new Cancion(
+                            ruta.getChildNodes().item(0).getNodeValue(),
+                            autor.getChildNodes().item(0).getNodeValue()
+                                               );
+                   // Cancion.put(ruta,autor);
+                    System.out.println(listaCanciones);
+                }
+            }
+        } catch (SAXException ex) {
+            ex.printStackTrace();
         }
     }
+
 }
